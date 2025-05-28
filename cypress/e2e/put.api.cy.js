21942f048f7521ccb939473ad9b7c3aae441dd7f
Luana Nascimento <luancmnt@gmail.com>
2025-05-28 17:05:08 +0100

    edits

--- cypress/e2e/put.api.cy.js
+++ cypress/e2e/put.api.cy.js
@@ -4,22 +4,33 @@ describe('Edit devices', () => {
 
     const body_post = require('../fixtures/create_device.json')
     const body_put = require('../fixtures/update_device.json')
-    const id = response_post.body.id
 
 
     it('Edit a device', () => {
 
         const currentDate = new Date().toISOString().slice(0, 16)
 
-        cy.postDevice(body_post)
-        .then((response_post) => {
+        cy.request({
+            method: 'POST',
+            url: '/objects',
+            failOnStatusCode: false,
+            body: body_post
+        }).as('postDeviceResult')
+
+        cy.get('@postDeviceResult').then((response_post) => {
             expect(response_post.status).equal(200)
             expect(response_post.body.name).equal(body_post.name)
             expect(response_post.body.data.price).equal(body_post.data.price)
 
 
-        cy.putDevice(body_put, id)
-        .then((response_put) => {
+        cy.request({
+            method: 'PUT',
+            url: `/objects/${response_post.body.id}`,
+            failOnStatusCode: false,
+            body: body_put
+        }).as('putDeviceResult')
+
+        cy.get('@putDeviceResult').then((response_put) => {
             expect(response_put.status).equal(200)
             expect(response_put.body.name).equal(body_put.name)
             expect(response_put.body.data.price).equal(body_put.data.price)
